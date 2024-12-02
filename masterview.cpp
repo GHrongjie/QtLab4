08632743d41713d86196d010a1119e2d24abc5fc
rongjie <shengrongjie666@163.com>
2024-12-02 14:49:57 +0800

    add basic jump to views

--- masterview.cpp
+++ masterview.cpp
@@ -1,14 +1,79 @@
 #include "masterview.h"
 #include "ui_masterview.h"
 
+#include <QDebug>
+
 MasterView::MasterView(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MasterView)
 {
     ui->setupUi(this);
+
+    goLoginView();
 }
 
 MasterView::~MasterView()
 {
     delete ui;
+}
+
+void MasterView::goLoginView()
+{
+    qDebug()<<"goLoginView";
+    loginView = new LoginView();
+
+    pushWidgetToStackView(loginView);
+}
+
+void MasterView::goWelcomeView()
+{
+    qDebug()<<"goWelcomeView";
+    welcomeView = new WelcomeView();
+
+    pushWidgetToStackView(welcomeView);
+}
+
+void MasterView::goDoctorView()
+{
+    qDebug()<<"goDoctorView";
+    doctorView = new DoctorView();
+
+    pushWidgetToStackView(doctorView);
+}
+
+void MasterView::goPatientView()
+{
+    qDebug()<<"goPatientView";
+    patientView = new PatientView();
+
+    pushWidgetToStackView(patientView);
+}
+
+void MasterView::goDepartmentView()
+{
+    qDebug()<<"goDepartmentView";
+    departmentView = new DepartmentView();
+
+    pushWidgetToStackView(departmentView);
+}
+
+void MasterView::goPatientEditView()
+{
+    qDebug()<<"goPatientEditView";
+    patientEditView = new PatientEditView();
+
+    pushWidgetToStackView(patientEditView);
+}
+
+void MasterView::goPreviousView()
+{
+
+}
+
+void MasterView::pushWidgetToStackView(QWidget *widget)
+{
+    ui->stackedWidget->addWidget(widget);
+    int count = ui->stackedWidget->count();
+    ui->stackedWidget->setCurrentIndex(count-1);//总是显示最新加入的view
+    ui->labelTitle->setText(loginView->windowTitle());
 }
--- masterview.h
+++ masterview.h
@@ -2,6 +2,12 @@
 #define MASTERVIEW_H
 
 #include <QMainWindow>
+#include "loginview.h"
+#include "welcomeview.h"
+#include "doctorview.h"
+#include "departmentview.h"
+#include "patientview.h"
+#include "patienteditview.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -17,7 +23,25 @@ public:
     MasterView(QWidget *parent = nullptr);
     ~MasterView();
 
+public slots:
+    void goLoginView();
+    void goWelcomeView();
+    void goDoctorView();
+    void goPatientView();
+    void goDepartmentView();
+    void goPatientEditView();
+    void goPreviousView();
+
 private:
+    void pushWidgetToStackView(QWidget *widget);
+
     Ui::MasterView *ui;
+
+    WelcomeView *welcomeView;
+    DoctorView *doctorView;
+    PatientView *patientView;
+    DepartmentView *departmentView;
+    LoginView *loginView;
+    PatientEditView *patientEditView;
 };
 #endif // MASTERVIEW_H
